8209eb699a086f1a8ed0bd34e1c0ce2801044e9a
Hamza <337118@student.mboutrecht.nl>
2024-01-07 22:13:13 +0100

    changed conig to new setting

--- app/config/config.php
+++ app/config/config.php
@@ -4,4 +4,4 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'be-opdracht1-j2-p1');
-define('URLROOT', 'http://www.be-opdracht3-j2-p1.com');
+define('URLROOT', 'http://www.be-p4-opdracht-04.com');
